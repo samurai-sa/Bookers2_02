841a2921d8b8f3ae636c7e72e4224566369be910
samurai-sa <football03xtk@gmail.com>
2021-07-07 01:47:56 +0000

    :zap: コメント非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,16 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
-    book = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    book.destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get "home/about" => "home#about"
-  resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
+  
+  resources :books, except:[:new] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
